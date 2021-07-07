5124e4a2392e277829bac99ae8a82e28542037bf
bskr2020 <bskr.2020@gmail.com>
2021-07-07 05:38:01 +0530

    adding files

---
+++ Student.java
@@ -0,0 +1,6 @@
+class Student{
+    public static void main(String[] args) {
+        system.out.Println("Hello , This is my program in inteliiJ IDE");
+        system.out.Println("Hello , This is my program in eclipse IDE");
+    }
+}
